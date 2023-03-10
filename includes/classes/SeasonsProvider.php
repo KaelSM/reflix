fe9feaa11b0f310209589a076966abd83f3ae3ee
KaelSM <kaelsebastian.moreiragonzalez@student.sl.on.ca>
2023-03-09 20:27:25 -0500

    created seasons class

---
+++ includes/classes/SeasonsProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+    class SeasonProvider {
+        private $con, $username;
+
+        public function __construct($con, $username) {
+            $this->con = $con;
+            $this->username = $username;
+        }
+
+        public function create($entity){
+            
+        }
+    
+    }
+
+?>
