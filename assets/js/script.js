0930ef1f3571a51290ab30d364ca0506b14c0689
KaelSM <kaelsebastian.moreiragonzalez@student.sl.on.ca>
2023-03-10 01:03:51 -0500

    adding functions to play video

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,48 @@ function previewEnded() {
     $(".previewVideo").toggle();
     $(".previewImage").toggle();
 }
+
+function goBack() {
+    window.history.back();
+}
+
+function startHideTimer() {
+    var timeout = null;
+    
+    $(document).on("mousemove", function() {
+        clearTimeout(timeout);
+        $(".watchNav").fadeIn();
+
+        timeout = setTimeout(function() {
+            $(".watchNav").fadeOut();
+        }, 2000);
+    })
+}
+
+function initVideo(videoId, username) {
+    startHideTimer();
+    updateProgressTimer(videoId, username);
+}
+
+function updateProgressTimer(videoId, username) {
+    addDuration(videoId, username);
+
+    var timer;
+    $("video").on("playing"), function(event){
+        window.clearInterval(timer);
+        timer = window.setInterval(function (){
+            console.log("hi");
+        }, 3000);
+    }
+    .on("ended", function() {
+        window.clearInterval(timer);
+    })
+}
+
+function addDuration(videoId, username) {
+    $.post("ajax/addDuration.php", { videoId: videoId, username: username }, function(data) {
+        if(data !== null && data !== "") {
+            alert(data);
+        }
+    })
+}
---
+++ watch.php
@@ -0,0 +1,28 @@
+<?php
+    require_once("includes/header.php");
+
+    if(!isset($_GET["id"])) {
+        ErrorMessage::show("no video found with selection");
+    }
+
+    $video = new Video($con, $_GET["id"]);
+    $video->incrementViews();
+
+    
+
+?>
+
+<div class="watchContainer">
+
+    <div class="videoControls watchNav">
+        <button onclick="goBack()"><i class="fas fa-arrow-left"></i></button>
+        <h1> <?php echo $video->getTitle(); ?></h1> </h1>
+
+    </div>
+    <video controls autoplay>
+        <source src='<?php echo $video->getFilePath(); ?>' type='video/mp4' >
+    </video>
+</div>
+<script>
+    initVideo("<?php echo $video->getId(); ?>", "<?php echo $userLoggedIn; ?>");
+</script>
